44d4cab8bed5d81fa005354d205881189b97092e
Pedro Costa <pcesar_costa@hotmail.com>
2026-04-07 16:06:23 -0300

    DECAF-211 Add heartbeat event handler (#16)

--- src/event/types.ts
+++ src/event/types.ts
@@ -1,4 +1,14 @@
 import { BulkCrudOperationKeys, OperationKeys } from "@decaf-ts/db-decorators";
+import { SseMessage } from "event-source-plus";
+
+export type ServerEventType = "message" | "heartbeat" | string;
+
+export type ServerRawMessage = SseMessage & {
+  id?: string;
+  event: ServerEventType;
+  data: string;
+  retry?: number;
+};
 
 export type ServerEvent = readonly [
   modelName: string,
