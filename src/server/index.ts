5d41f02dbb26bac34061e25574aae9faf40387a7
tvenceslau <venceslau.tiago@gmail.com>
2026-06-19 14:23:06 +0100

    DECAF-798 - more sever logic - moves hooks to own export

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import "./overrides";
 
 export * from "./controllers";
-export * from "./hooks";
 export * from "./overrides";
+export * from "./constants";
+export * from "./decorators";
+export * from "./types";
